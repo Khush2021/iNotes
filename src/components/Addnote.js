da801e9e7f51b7ac92e34a07aa376378e50a1bfb
Khush Vijayvarigya <93064601+Khush2021@users.noreply.github.com>
2024-04-30 00:45:44 +0530

    changes

--- src/components/Addnote.js
+++ src/components/Addnote.js
@@ -1,16 +1,16 @@
 import React, { useState, useContext } from "react";
 import NoteContext from "../context/notes/notecontext";
-import Select from 'react-select'
+import Select from "react-select";
 
 const options = [
-  {value: 'General', label: 'General'},
-  {value: 'Study', label: 'Study'},
-  {value: 'Research', label: 'Research'},
-  {value: 'Meeting', label: 'Meeting'},
-  {value: 'Ideas', label: 'Ideas'},
-  {value: 'Goals', label: 'Goals'},
-  {value: 'Project', label: 'Project'},
-]
+  { value: "General", label: "General" },
+  { value: "Study", label: "Study" },
+  { value: "Research", label: "Research" },
+  { value: "Meeting", label: "Meeting" },
+  { value: "Ideas", label: "Ideas" },
+  { value: "Goals", label: "Goals" },
+  { value: "Project", label: "Project" },
+];
 
 const Addnote = () => {
   const noteContext = useContext(NoteContext);
@@ -19,22 +19,34 @@ const Addnote = () => {
   const [note, setNote] = useState({
     title: "",
     description: "",
-    tags: [{
-      label: "General",
-      value: "General"
-    }],
+    tags: [
+      {
+        label: "General",
+        value: "General",
+      },
+    ],
   });
   const handleClick = (event) => {
     event.preventDefault();
-    let tags = note.tags.map((tag) => tag.value)
+    if (note.title.length === 0) {
+      alert("title length should be greater than 0!");
+      return;
+    }
+    if (note.description.length < 5) {
+      alert("Description must be atleast 5 characters");
+      return;
+    }
+    let tags = note.tags.map((tag) => tag.value);
     addNote(note.title, note.description, tags);
     setNote({
       title: "",
       description: "",
-      tags: [{
-        label: "General",
-        value: "General"
-      }],
+      tags: [
+        {
+          label: "General",
+          value: "General",
+        },
+      ],
     });
   };
   const onchange = (e) => {
@@ -43,15 +55,17 @@ const Addnote = () => {
   const handleTagChange = (e) => {
     setNote({
       ...note,
-      tags: e
-    })
-  }
+      tags: e,
+    });
+  };
   return (
     <div className="container my-3">
       <h1>Add a note</h1>
       <form className="my-3">
         <div className="form-group">
-          <label htmlFor="title">Title<sup style={{color: 'red'}}>*</sup></label>
+          <label htmlFor="title">
+            Title<sup style={{ color: "red" }}>*</sup>
+          </label>
           <input
             type="text"
             className="form-control"
@@ -64,7 +78,9 @@ const Addnote = () => {
           />
         </div>
         <div className="form-group">
-          <label htmlFor="description">Description<sup style={{color: 'red'}}>*</sup></label>
+          <label htmlFor="description">
+            Description<sup style={{ color: "red" }}>*</sup>
+          </label>
           <textarea
             type="text"
             className="form-control"
@@ -73,15 +89,24 @@ const Addnote = () => {
             name="description"
             value={note.description}
             onChange={onchange}
-            style={{ height: "100px" , maxHeight: "300px", textAlign: "initial"}}
+            style={{
+              height: "100px",
+              maxHeight: "300px",
+              textAlign: "initial",
+            }}
           />
         </div>
         <div className="form-group">
           <label htmlFor="tag">Tags(optional)</label>
-          <Select options={options} isMulti name="tag" id="tag" onChange={handleTagChange}/>
+          <Select
+            options={options}
+            isMulti
+            name="tag"
+            id="tag"
+            onChange={handleTagChange}
+          />
         </div>
         <button
-          disabled={note.title.length === 0 || note.description.length <  5}
           type="submit"
           className="btn btn-primary my-3"
           id="add-note-btn"
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -7,37 +7,42 @@ const Noteitem = (props) => {
   const noteContext = useContext(NoteContext);
   const { delNote } = noteContext;
   const handleClick = () => {
-    delNote(note?._id);
+    if (delNote(note?._id)) alert("note deleted!");
   };
   return (
     <div className="col-md-5">
       <div className="card my-3">
         <div className="card-body">
-          <div className="d-flex align-items-center" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+          <div
+            className="d-flex align-items-center"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
             <h5 className="card-title">{note?.title}</h5>
             <div>
-            <i className="fa-solid fa-trash mx-2" onClick={handleClick}></i>
-            <i
-              className="fa-solid fa-pen-to-square mx-2"
-              onClick={() => {
-                updateNote();
-              }}
-            ></i>
+              <i className="fa-solid fa-trash mx-2" onClick={handleClick}></i>
+              <i
+                className="fa-solid fa-pen-to-square mx-2"
+                onClick={() => {
+                  updateNote();
+                }}
+              ></i>
             </div>
           </div>
           <p className="card-text">{note?.description}</p>
-        <div style={{display: 'flex', flexDirection: 'row', gap: '10px'}}>
-          {note?.tags?.map((tag) => {
-            return (
-              <span
-                className="badge bg-success px-2"
-                key={tag}
-              >
-                {tag}
-              </span>
-            );
-          })}
-        </div>
+          <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+            {note?.tags?.map((tag) => {
+              return (
+                <span className="badge bg-success px-2" key={tag}>
+                  {tag}
+                </span>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
